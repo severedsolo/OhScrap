4aef790f1376d001c06ebfe269b7429d86aee6b5
severedsolo <evil.penguin@live.co.uk>
2017-05-21 19:56:38 +0100

    Revert "Cant remember what this is"
    This reverts commit 9e5821eed85e1a66f777e7052ed532ac3d0366cd.

--- UPFM/Untitled Part Failure Mod/BaseFailureModule.cs
+++ UPFM/Untitled Part Failure Mod/BaseFailureModule.cs
@@ -14,7 +14,7 @@ namespace Untitled_Part_Failure_Mod
         public bool hasFailed = false;
         public float expectedLifetime = 2;
         ModuleSYPartTracker SYP;
-        double chanceOfFailure = 0.5f;
+        float chanceOfFailure = 0.5f;
         [KSPField(isPersistant = true, guiActive = true, guiName = "Generation")]
         public int generation = 0;
         double failureTime = 0;
@@ -96,7 +96,7 @@ namespace Untitled_Part_Failure_Mod
             if (recalcChance)
             {
                 chanceOfFailure = chanceOfFailure / generation;
-                chanceOfFailure = 1 - (1 - chanceOfFailure) * Math.Exp(-SYP.TimesRecovered / expectedLifetime); ;
+                if (SYP.TimesRecovered > 0) chanceOfFailure = chanceOfFailure * ((SYP.TimesRecovered / expectedLifetime));
 
             }
             Debug.Log("[UPFM]: Chances of "+part.name+" failing calculated to be " + chanceOfFailure * 100 + "%");
