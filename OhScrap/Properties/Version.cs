f950165fe96198a8bcf4b59ca17c465eb7b1c310
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2020-03-12 22:11:25 -0500

    2.1.1.0

--- OhScrap/Properties/Version.cs
+++ OhScrap/Properties/Version.cs
@@ -7,9 +7,9 @@ namespace OhScrap
     {
         public const int major = 2;
         public const int minor = 1;
-        public const int patch = 0;
+        public const int patch = 1;
         public const int build = 0;
-        public const string Number = "2.1.0.0";
+        public const string Number = "2.1.1.0";
 #if DEBUG
         public const string Text = Number + " BETA DEBUG";
 #else
