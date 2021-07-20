732c7bc643e2d07051411fb80bb014797aeb7e93
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2021-07-19 22:53:14 -0500

    rebuild for KSP 1.12.x

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OhScrap")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Scrapyard based Part Failure addin for Kerbal Space Program.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("KGEx")]
 [assembly: AssemblyProduct("OhScrap")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
--- Source/Properties/Version.cs
+++ Source/Properties/Version.cs
@@ -6,10 +6,10 @@ namespace OhScrap
     public static class Version
     {
         public const int major = 2;
-        public const int minor = 1;
-        public const int patch = 1;
+        public const int minor = 2;
+        public const int patch = 0;
         public const int build = 0;
-        public const string Number = "2.1.1.0";
+        public const string Number = "2.2.0.0";
 #if DEBUG
         public const string Text = Number + " BETA DEBUG";
 #else
